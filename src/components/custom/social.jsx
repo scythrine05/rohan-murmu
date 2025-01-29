f1ec49ac24005532d1f4bf999aa7958b998bea68
Scythrine 05 | Rohan Murmu <scythrine05@gmail.com>
2025-01-29 21:41:09 +0530

    Added linksData

--- src/components/custom/social.jsx
+++ src/components/custom/social.jsx
@@ -1,27 +1,24 @@
 import React from "react";
-
-import { FaGithub, FaFacebook, FaLinkedin, FaFile } from "react-icons/fa";
+import { linkData } from "../../data/links-data";
 
 import "../../styles/social.css";
 
 export default function Social() {
   return (
     <div className="social-group">
-      <a href="https://www.linkedin.com/in/rohan-murmu/" target="_blank">
-        <FaLinkedin size={26} />
-      </a>
-      <a href="https://github.com/scythrine05" target="_blank">
-        <FaGithub size={26} />
-      </a>
-      <a href="https://www.facebook.com/rohan.murmu.39/" target="_blank">
-        <FaFacebook size={26} />
-      </a>
-      <a
-        href="https://drive.google.com/file/d/1e9D-ouF2phBDi-GMjxC7Lnk2pzo_oi4Z/view?usp=sharing"
-        target="_blank"
-      >
-        <FaFile size={24} />
-      </a>
+      {linkData.map((link, index) => {
+        return (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="social-link"
+          >
+            {link.icon}
+          </a>
+        );
+      })}
     </div>
   );
 }
---
+++ src/data/links-data.jsx
@@ -0,0 +1,20 @@
+import { FaGithub, FaFacebook, FaLinkedin, FaFile } from "react-icons/fa";
+
+const linkData = [
+  {
+    url: "https://www.linkedin.com/in/rohan-murmu/",
+    icon: <FaLinkedin size={26} />,
+  },
+  {
+    url: "https://github.com/scythrine05",
+    icon: <FaGithub size={26} />,
+  },  {
+    url: "https://www.facebook.com/rohan.murmu.39/",
+    icon: <FaFacebook size={26} />,
+  },  {
+    url: "https://drive.google.com/file/d/10VR6_R_sL3qXkEPA0YJrhh1Wp4F18chX/view?usp=sharing",
+    icon: <FaFile size={26} />,
+  },
+];
+
+export { linkData };
